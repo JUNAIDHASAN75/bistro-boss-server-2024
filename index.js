014d3f99fa84baf38289a5a2b19f89376e347bda
Junaid Hasan <spiderss907@gmail.com>
2024-10-27 13:52:57 +0600

    few changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.rm5ydmz.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,17 +24,46 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menuCollection = client.db("bistroBossDb").collection("menu");
+    const cartsCollection = client.db("bistroBossDb").collection("carts");
     // Send a ping to confirm a successful connection
 
     app.get("/menu", async (req, res) => {
-        try {
-          const result = await menuCollection.find().toArray();
-          res.send(result);
-        } catch (error) {
-          console.error("Error fetching menu:", error);
-          res.status(500).json({ error: "Internal Server Error" });
-        }
-      });
+      const data = req;
+      console.log(data)
+      try {
+        const result = await menuCollection.find().toArray();
+
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching menu:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: id };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = cartsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
